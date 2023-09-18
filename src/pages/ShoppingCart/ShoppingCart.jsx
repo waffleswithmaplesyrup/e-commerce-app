f6cc505fa3fa1b367b3205cb973d7615ce3a7cbd
Vakarian <fazmn7@gmail.com>
2023-09-18 21:56:08 +0800

    managed to PATCH air table data ^U^

--- src/pages/ShoppingCart/ShoppingCart.jsx
+++ src/pages/ShoppingCart/ShoppingCart.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import PurchasingCard from "../components/PurchasingCard";
+
+const TOKEN =
+  "patjEsSqznCeKP4Tm.3498c9f837410cce65c273bfb3b6ea7b4203984e08ac1a3ed63de377edd64eaf";
+const BASE_URL = "https://api.airtable.com/v0/app7Fu8VNb6BUxYbM";
+
 export default function ShoppingCart() {
-  return <div>
-    shopping cart page
-  </div>
+
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const fetchCart = async () => {
+      const response = await fetch(`${BASE_URL}/cart`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      });
+      const jsonData = await response.json();
+      const cartData = jsonData.records.map((data) => ({
+        ...data.fields,
+        id: data.id,
+      }));
+      setCartItems(cartData);
+      // console.log(cartData);
+
+    };
+    fetchCart();
+  }, []);
+
+  return <>
+  {/* render stored data from air table */}
+  {cartItems?.map(item => <PurchasingCard key={item.id} item={item} />)}
+  
+  </>
 }
---
+++ src/pages/components/PurchasingCard.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import StarRating from "./StarRating";
+
+const TOKEN =
+  "patjEsSqznCeKP4Tm.3498c9f837410cce65c273bfb3b6ea7b4203984e08ac1a3ed63de377edd64eaf";
+const BASE_URL = "https://api.airtable.com/v0/app7Fu8VNb6BUxYbM";
+
+export default function PurchasingCard({ item }) {
+  const [quantity, setQuantity] = useState(item.quantity);
+
+  const handleChange = async (event) => {
+    console.log(event.target.value);
+    setQuantity(event.target.value);
+
+    const payload = {
+      fields:{
+        quantity: event.target.value,
+      }
+    };
+
+    const response = await fetch(`${BASE_URL}/cart/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      body: JSON.stringify(payload)
+    });
+    await response.json();
+
+  };
+
+  const handleClick = () => {
+    console.log(`remove ${item.title} from cart`);
+  };
+
+  return <div className="purchasing">
+  <img src={item.thumbnail} alt={item.id} />
+  <h3>{item.title}</h3>
+  <StarRating score={item.rating} />
+  <p>quantity:</p>
+  <input type="number" min="1" max={item.stocks} value={quantity} onChange={handleChange}/>
+  <p>stock left: {item.stocks-quantity}</p>
+  <button onClick={handleClick}>remove from cart</button>
+  </div>
+}
